7fd06e565c718265bacd7fce36dc290c4ddff735
Gustavo <ghferreira92@gmail.com>
2023-09-16 00:49:32 -0300

    push de segurança

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Arvore_binaria.hpp"
 #include "Arvore_AVL.hpp"
 #include "HeapMAX.hpp"
+#include <algorithm>
 #include <chrono>
 
 #define NUM_SUGESTOES 5
@@ -74,26 +75,39 @@ int main() {
 
                     // ^^^ verifica se a palavra está no texto e armazena a frequencia
 
-                    HeapMAX Heap;
+                    HeapMAX Heap_bt;
+                    HeapMAX Heap_avl;
 
                     for (const auto &item : frequencia) {
-                        Heap.inserir(DataPair(item.first, item.second));
+                        Heap_bt.inserir(DataPair(item.first, item.second));
                     }
 
-                    Heap.RemoveSugestao(palavra_pesq, Heap, NUM_SUGESTOES);
+                    for (const auto &item : frequencia) {
+                        Heap_avl.inserir(DataPair(item.first, item.second));
+                    }
+
+
+                    Heap_bt.RemoveSugestao(palavra_pesq, Heap_bt, NUM_SUGESTOES);
+                    Heap_avl.RemoveSugestao(palavra_pesq, Heap_avl, NUM_SUGESTOES);
 
                     // ^^^ monta a heap e remove a palavra pesq das k mais freq se ela tiver lá
 
                     Arvore_binaria Binary_tree = Arvore_binaria();
                     Arvore_AVL AVL_tree = Arvore_AVL();
 
-                    for (int i = 0; i < NUM_SUGESTOES && !Heap.Vazia(); ++i) {
-                        DataPair pair = Heap.PesquisaMAX();
-                        Binary_tree.Inserir(pair.palavra, pair.freq);
-                        AVL_tree.Inserir(pair.palavra, pair.freq);
+                    for (int i = 0; i < NUM_SUGESTOES && !Heap_bt.Vazia(); ++i) {
+                        DataPair pair_bt = Heap_bt.PesquisaMAX();
+                        Binary_tree.Inserir(pair_bt.palavra, pair_bt.freq);
                     }
 
-                    Heap.cleanHEAP(Heap);
+                    for (int i = 0; i < NUM_SUGESTOES && !Heap_avl.Vazia(); ++i) {
+                        DataPair pair_avl = Heap_avl.PesquisaMAX();
+                        AVL_tree.Inserir(pair_avl.palavra, pair_avl.freq);
+                    }
+
+
+                    Heap_bt.cleanHEAP(Heap_bt);
+                    Heap_avl.cleanHEAP(Heap_avl);
 
                     // joga os k mais frequentes pras arvores e limpa a heap ^^^
 
